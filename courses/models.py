44706bd53a89ee231ea7fc232c725ab7b94f70f8
Mario <mariohdpz@gmail.com>
2020-01-14 19:55:15 -0600

    Step 3: Create models and make migrations

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,35 @@
+""" Blog models """
 from django.db import models
 
-# Create your models here.
+class Category(models.Model):
+    """ Category model to classify courses """
+    name = models.CharField(max_length=40)
+    slug = models.SlugField(max_length=60)
+
+    def __str__(self):
+        return self.name
+
+class Module(models.Model):
+    """ Modules of each course """
+    name = models.CharField(max_length=40)
+    description = models.TextField(blank=False)
+    sort_order = models.IntegerField(null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
+
+class Course(models.Model):
+    """ Course model """
+    name = models.CharField(max_length=100, blank=False)
+    description = models.TextField(blank=False)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+    modules = models.ManyToManyField(Module)
+    slug = models.SlugField(max_length=60, blank=True)
+    image_url = models.URLField(null=True)
+    created_at = models.DateTimeField(auto_now=True)
+    updated_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.name
