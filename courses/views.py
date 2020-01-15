683a3382ae49c36278c61563c6ca43a9c237809b
Mario <mariohdpz@gmail.com>
2020-01-14 20:14:58 -0600

    Step 5: Add home view and router

--- courses/views.py
+++ courses/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+""" Course views """
+from django.views.generic import ListView
 
-# Create your views here.
+from .models import Course
+
+class AllCourses(ListView):
+    """ Render all courses """
+    template_name = "courses_list.html"
+    context_object_name = "courses"
+    model = Course
